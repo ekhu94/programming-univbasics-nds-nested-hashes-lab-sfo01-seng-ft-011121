7999b1f117850badb30aa4d0d1f266bcc0570e25
Erik Huang <erikhuangdrums@gmail.com>
2020-12-17 16:56:23 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,31 +6,3 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
-  [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
-    ]
-end
-
-def literal_aoh
-  [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
-    ]
-end
-
-def aoh_lookup(aoh, row, key)
-  aoh[row][key]
-end
-
-def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] = new_value
-  aoh
-end
